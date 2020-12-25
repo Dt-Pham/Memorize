9384c36ed522a9ea1fb71ae36652092fe4e8235b
DT Pham <dt99.pham@gmail.com>
2020-12-25 10:02:53 -0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Duong Pham on 12/25/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

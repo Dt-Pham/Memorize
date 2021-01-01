95dfc5b4db69fa4679171434b7645895aedb5712
DT Pham <dt99.pham@gmail.com>
2020-12-31 16:45:50 -0800

    Created bonus time animation.

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -34,12 +34,32 @@ struct EmojiMemoryGameView: View {
 
 struct CardView: View, Animatable {
     var card: MemoryGame<String>.Card
+    
+    @State private var animatedBonusRemaining: Double = 0
+    
+    private func startBonusTimeAnimation() {
+        animatedBonusRemaining = card.bonusRemaining
+        withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+            animatedBonusRemaining = 0
+        }
+    }
+    
     var body: some View {
         GeometryReader { geometry in
             if !card.isMatched || card.isFaceUp {
                 ZStack {
-                    Pie(startAngle: Angle.degrees(-90), endAngle: Angle.degrees(60), clockwise: true)
-                        .opacity(0.5).padding()
+                    Group {
+                        if card.isConsumingBonusTime {
+                            Pie(startAngle: Angle.degrees(-90), endAngle: Angle.degrees(-animatedBonusRemaining * 360 - 90), clockwise: true)
+                                .onAppear {
+                                    startBonusTimeAnimation()
+                                }
+                        }
+                        else {
+                            Pie(startAngle: Angle.degrees(-90), endAngle: Angle.degrees(-card.bonusRemaining * 360 - 90), clockwise: true)
+                        }
+                    }
+                        .opacity(0.4).padding()
                     Text(card.content)
                         .rotationEffect(card.isMatched ? Angle.degrees(360) : Angle.degrees(0))
                         .animation(card.isMatched ? Animation.linear(duration: 1).repeatForever(autoreverses: false) : .default)
